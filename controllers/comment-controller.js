d52617466f1e4580a2d610d558371d10b1cb13c9
lnguyen87 <luc_nguyen@outlook.com>
2022-02-25 17:07:09 -0700

    added reply feature to reply to comments and connected to front end

--- controllers/comment-controller.js
+++ controllers/comment-controller.js
@@ -21,6 +21,32 @@ const commentController = {
           })
           .catch(err => res.json(err));
     },
+    // add reply to comment - POST route
+    addReply({ params, body }, res) {
+      Comment.findOneAndUpdate(
+        { _id: params.commentId },
+        { $push: { replies: body } },
+        { new: true }
+      )
+        .then(dbPizzaData => {
+          if (!dbPizzaData) {
+            res.status(404).json({ message: 'No pizza found with this id!' });
+            return;
+          }
+          res.json(dbPizzaData);
+        })
+        .catch(err => res.json(err));
+    },
+    // remove reply - DELETE route
+    removeReply({ params }, res) {
+      Comment.findOneAndUpdate(
+        { _id: params.commentId },
+        { $pull: { replies: { replyId: params.replyId } } },
+        { new: true }
+      )
+        .then(dbPizzaData => res.json(dbPizzaData))
+        .catch(err => res.json(err));
+    },
     // remove comment - DELETE route
     removeComment({ params }, res) {
         Comment.findOneAndDelete({ _id: params.commentId })
